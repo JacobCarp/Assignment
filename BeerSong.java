be7679b50a0fe3028dd579919fdacad57e8d4987
Jacob Carpenter <jtcarpenter@oakland.edu>
2016-09-30 16:11:18 -0500

    Fully Implemented BeerSong.java

--- BeerSong.java
+++ BeerSong.java
@@ -1,11 +1,12 @@
 public class BeerSong {
  final static String bottles1 = " bottles of beer on the wall ";
  final static String bottles2 = " bottles of beer, \n take one down pass it around, ";
-
+ 
  public static void main(String[] args) {
   // TODO code application logic here
   ninety_nine_bottles();
  }
+ //outputs song from 99 to 1
  public static void ninety_nine_bottles(){
   for(int i =99; i>0; i--){
    if(i==1){System.out.print(""+i+bottles1+i+bottles2+"there are no more bottles of beer on the wall");}
